d39022eabddc4ed01f4bbcaf02b50988c68682c3
Chris Taylor <beerobber@gmail.com>
2021-01-28 18:23:27 -0500

    Corrected misplaced hymn text location

--- db/fetch-texts.bash
+++ db/fetch-texts.bash
@@ -5,4 +5,4 @@
 # The full-texts S3 bucket, assumed to have one .txt file per hymn
 S3_BUCKET=osh-ce-full-texts
 
-aws s3 cp s3://${S3_BUCKET}/ db/texts/ --recursive;
+aws s3 cp s3://${S3_BUCKET}/ texts/ --recursive;
